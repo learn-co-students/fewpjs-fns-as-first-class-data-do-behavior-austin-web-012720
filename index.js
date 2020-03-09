ea93d87d3293f4393dae84e98b5e497bee9dd3eb
github username <github email address>
2020-03-09 14:22:49 -0500

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,24 @@
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
-}
+};
 
 /* End Given Code, don't edit above here...*/
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string;
+};
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(time) {
+  let string = time.split(':')[0];
+
+  let num = parseInt(string, 10);
+
+  if (num > 0 && num < 12) {
+    return "Good Morning";
+  } else if (num > 17) {
+    return "Good Evening";
+  } else {
+    return "Good Afternoon"; 
+  };
+};
+
