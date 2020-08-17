6b704ea3c675396baee356033e4b86ea3995ed8f
benbrower <bbrower@smu.edu>
2020-08-17 18:54:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 /* Given Code, don't edit */
 
+document.querySelector("button").addEventListener("click", handleClick);
+
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-function greet(time){
-  if (parseInt(time.split(':')[0]) < 12)
+function greet(str) {
+  time = parseInt(str.split(":"));
+  if (time < 12) {
     return "Good Morning";
-  else if (parseInt(time.split(':')[0]) >= 12 && parseInt(time.split(':')[0]) < 18)
+  } else if (time < 18) {
     return "Good Afternoon";
-  else return "Good Evening";
+  } else {
+    return "Good Evening";
+  }
+  // return parseInt(str.split(":")) < 12 ? "Good Morning" : "Good Afternoon";
+  // switch (parseInt(str.split(":")) < 12) {
+  //   case true:
+  //     return "Good Morning";
+  //   case false:
+  //     return "Good Afternoon";
+  // }
 }
 
 /* Write your implementation of displayMessage() */
-function displayMessage(message){
-  document.getElementById('greeting').innerText = message;
+function displayMessage(str) {
+  console.log(document.getElementById("greeting"));
+  document.getElementById("greeting").innerText = str;
 }
